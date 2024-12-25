c2da62a342ba9f8ad6fd555ae2aec214c27adf27
Saya1206 <sayali.patil0831>
2024-12-25 09:10:09 +0530

    Even Odd

---
+++ 2_evenOdd.js
@@ -0,0 +1,10 @@
+let check = (num) => {
+  if (num % 2 == 0) {
+    console.log(`Number ${num} is Even`);
+  } else {
+    console.log(`Number ${num} is Odd`);
+  }
+};
+
+check(472);
+check(93287);
