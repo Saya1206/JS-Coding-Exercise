735be4832054db0f53dd77eb15215b72a2775940
Saya1206 <sayali.patil0831>
2025-01-01 23:34:17 +0530

    Sort array of strings

---
+++ 19_sortArrayOfString.js
@@ -0,0 +1,7 @@
+let flowers = ["Rose", "Jasmin", "Chapha", "Lotus", "Tulip", "Lily", "Chameli"];
+
+let ascending = flowers.sort();
+console.log(ascending);
+
+let descending = flowers.sort().reverse();
+console.log(descending);
