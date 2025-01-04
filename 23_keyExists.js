bbafec8bc0d23530b59e441d24791a698cd0efd8
Saya1206 <sayali.patil0831>
2025-01-04 13:26:03 +0530

    Check if a key exists

---
+++ 23_keyExists.js
@@ -0,0 +1,11 @@
+//Check if a key exists in an object
+
+let person = {
+  id: 1,
+  name: "ajay",
+  isEmployee: true,
+  salary: 40000,
+};
+
+console.log("name" in person);
+console.log("address" in person);
