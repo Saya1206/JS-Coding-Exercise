d093399eb1a7d0f137fc3a58e7a688aa5fc9ceed
Saya1206 <sayali.patil0831>
2025-01-05 22:09:38 +0530

    Count Keys

---
+++ 26_countKeys.js
@@ -0,0 +1,11 @@
+//Find the number of keys in an object
+
+let emp = {
+  id: 1,
+  name: "sakshi",
+  salary: 40000,
+  city: "pune",
+};
+
+let keyCount = Object.keys(emp).length;
+console.log(keyCount);
