00a0a523d20e250ee08e78fecb5493f75aa97143
Saya1206 <sayali.patil0831>
2025-03-02 23:17:53 +0530

    convert object to key-value pairs

---
+++ 29_convertArray.js
@@ -0,0 +1,7 @@
+// Convert an object to an array of key-value pairs
+
+const obj = { name: "neha", id: 35, city: "kolhapur" };
+
+const keyValuePairs = Object.entries(obj);
+
+console.log(keyValuePairs);
