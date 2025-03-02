faee3daa23de66fd1f2423df99f400fe5de1d066
Saya1206 <sayali.patil0831>
2025-03-02 23:25:11 +0530

    Extract values from object

---
+++ 30_extractValues.js
@@ -0,0 +1,10 @@
+// Extract values from an object into an array
+
+const obj = { name: "neha", id: 35, city: "kolhapur" };
+
+let values = Object.values(obj);
+
+console.log(values);
+
+// Looping through values:
+// Object.values(obj).forEach((value) => console.log(value));
