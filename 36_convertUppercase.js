71d91a7f35bde291033154774486c78c6a3a37f2
Saya1206 <sayali.patil0831>
2025-07-08 23:18:13 +0530

    Convert a string to uppercase

---
+++ 36_convertUppercase.js
@@ -0,0 +1,7 @@
+let str = "rakhi";
+let upper = str.toUpperCase();
+console.log("Upper Case: " + upper);
+
+let str2 = "RoHan";
+let lower = str2.toLowerCase();
+console.log("Lower Case: " + lower);
